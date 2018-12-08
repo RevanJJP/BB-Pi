a0347ade4e740de45ca13110553dd1fd2098533d
Jakub Posłuszny <revanjjp@gmail.com>
2018-12-08 12:02:11 +0100

    Updated header file

--- i2c_wrapper.h
+++ i2c_wrapper.h
@@ -1,7 +1,19 @@
 int init_i2c();
+
 int reset_i2c();
-void set_scl (int frequency);
-int get_scl_freq();
+
+void set_scl_high (int frequency);
+
+void set_scl_low (int frequency);
+
+int read_scl();
+
 void set_sda_high ();
+
 void set_sda_low ();
+
+void set_input_sda();
+
+void set_output_sda();
+
 int read_sda();
